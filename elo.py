923b307cc07ae5b140808311e851b2a91e5a2d45
Mateo Velazquez <mateovg@gmail.com>
2024-07-18 10:32:48 -0400

    elo refactoring

--- elo.py
+++ elo.py
@@ -1,32 +1,32 @@
 # functions to calculate each part of the elo formula
 
 class Elo:
-    def __init__(self, K_VALUE=20, HOME_ADVANTAGE=100):
-        self.K_VALUE = K_VALUE
-        self.HOME_ADVANTAGE = HOME_ADVANTAGE
+    def __init__(self, df: DataFrame, k_factor=20, home_advantage=100):
+        self.k_factor = k_factor
+        self.home_advantage = home_advantage
 
-    def find_K(MOV, elo_diff):
+    def find_K(self, margin_of_victory: int, elo_diff: int) -> int:
         # K is multiplier to apply to the elo change
         # the multiplier function is from 538, as is the k_0 value
         # positive MOV means home team won
-        K_0 = self.K_VALUE
-        if MOV > 0:
-            multiplier = (MOV + 3) ** 0.8 / (7.5 + 0.006 * elo_diff)
+        k_factor = self.k_factor
+        if margin_of_victory > 0:
+            multiplier = (margin_of_victory + 3) ** 0.8 / (7.5 + 0.006 * elo_diff)
         else:
-            multiplier = (-MOV + 3) ** 0.8 / (7.5 + 0.006 * -elo_diff)
+            multiplier = (-margin_of_victory + 3) ** 0.8 / (7.5 + 0.006 * -elo_diff)
         # returns two values, one for each team
-        return K_0 * multiplier, K_0 * multiplier
+        return k_factor * multiplier, k_factor * multiplier
 
-    def find_S(MOV):
-        if MOV > 0:  # home team won
+    def find_S(self, margin_of_victory: int):
+        if margin_of_victory > 0:  # home team won
             return 1, 0
-        elif MOV < 0:  # home team lost
+        elif margin_of_victory < 0:  # home team lost
             return 0, 1
         else:
             return 0.5, 0.5  # tie
 
-    def find_E(elo_home, elo_away):
-        home_adv = self.HOME_ADVANTAGE
+    def find_E(self, elo_home: int, elo_away: int) -> tuple[float, float]:
+        home_adv = self.home_advantage
         elo_home += home_adv
 
         E_home = 1 / (1 + 10 ** ((elo_away - elo_home) / 400.0))
@@ -34,23 +34,23 @@ class Elo:
 
         return E_home, E_away
 
-    def find_new_elo(elo_home, elo_away, MOV):
+    def find_new_elo(self, elo_home, elo_away, margin_of_victory):
         elo_diff = elo_home - elo_away
-        K_home, K_away = find_K(MOV, elo_diff)
-        S_home, S_away = find_S(MOV)
-        E_home, E_away = find_E(elo_home, elo_away)
+        K_home, K_away = self.find_K(margin_of_victory, elo_diff)
+        S_home, S_away = self.find_S(margin_of_victory)
+        E_home, E_away = self.find_E(elo_home, elo_away)
 
         elo_home = elo_home + K_home * (S_home - E_home)
         elo_away = elo_away + K_away * (S_away - E_away)
 
         return elo_home, elo_away
 
-    def find_decay_elo(elo):
+    def find_decay_elo(self, elo):
         # 1505 to aim for a 1500 mean
         # due to new teams joining the league
         return elo * 0.75 + 1505 * 0.25
 
-    def find_elo_ratings(season, elo_dict):
+    def find_elo_ratings(self, season, elo_dict):
         # loop through each game and calculate the new elo rating for each team
         # each row should have the elo rating BEFORE the game
 
@@ -66,7 +66,8 @@ class Elo:
 
             MOV = row['plus_minus_home']
 
-            elo_home_new, elo_away_new = find_new_elo(elo_home, elo_away, MOV)
+            elo_home_new, elo_away_new = self.find_new_elo(
+                elo_home, elo_away, MOV)
 
             elo_dict[home_team] = elo_home_new
             elo_dict[away_team] = elo_away_new
